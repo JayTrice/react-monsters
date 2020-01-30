2a0efd5187406d5ab64ff7369886c767cb7ea2b1
JayTrice <support@tokenoasis.com>
2020-01-29 22:21:01 -0500

    Updates

---
+++ precache-manifest.a6e8fb8337356f5fc9e8483ae596be53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4da088e642538e810b416f8431bb98",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "a3d14a99bd87f68d8e18",
+    "url": "/react-monsters/static/css/main.1298975b.chunk.css"
+  },
+  {
+    "revision": "364c6a7741c1458d55ea",
+    "url": "/react-monsters/static/js/2.12430192.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monsters/static/js/2.12430192.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3d14a99bd87f68d8e18",
+    "url": "/react-monsters/static/js/main.9a96e244.chunk.js"
+  },
+  {
+    "revision": "8045bb47f0578851891b",
+    "url": "/react-monsters/static/js/runtime-main.388beaee.js"
+  }
+]);
